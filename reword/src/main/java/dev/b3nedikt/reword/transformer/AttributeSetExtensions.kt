eb27b7f76e4368d4f85344454ade34286c8cd953
B3nedikt <mail@b3nedikt.dev>
2019-12-30 16:08:36 +0100

    Updated docs

--- reword/src/main/java/dev/b3nedikt/reword/transformer/AttributeSetExtensions.kt
+++ reword/src/main/java/dev/b3nedikt/reword/transformer/AttributeSetExtensions.kt
@@ -2,6 +2,12 @@ package dev.b3nedikt.reword.transformer
 
 import android.util.AttributeSet
 
+/**
+ * Returns a [Map] containing key-value pairs provided by [transform] function
+ * applied to elements of the given [AttributeSet].
+ *
+ * If any of two pairs would have the same key the last one gets added to the map.
+ */
 fun <K, V> AttributeSet.associate(transform: (index: Int) -> Pair<K, V>): Map<K, V>  {
     return (0 until attributeCount).associate(transform)
 }
