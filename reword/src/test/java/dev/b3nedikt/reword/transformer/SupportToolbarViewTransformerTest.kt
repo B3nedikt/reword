ab7ec722d9e210e707ad69dde5d443be785349c4
B3nedikt <mail@b3nedikt.dev>
2020-01-09 21:24:33 +0100

    Improved unit tests

--- reword/src/test/java/dev/b3nedikt/reword/transformer/SupportToolbarViewTransformerTest.kt
+++ reword/src/test/java/dev/b3nedikt/reword/transformer/SupportToolbarViewTransformerTest.kt
@@ -3,18 +3,14 @@ package dev.b3nedikt.reword.transformer
 import android.content.Context
 import android.content.res.Resources
 import android.os.Build
-import android.util.AttributeSet
 import androidx.appcompat.widget.Toolbar
 import androidx.test.core.app.ApplicationProvider
 import com.nhaarman.mockitokotlin2.doReturn
-import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.spy
 import com.nhaarman.mockitokotlin2.whenever
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers.anyInt
-import org.mockito.ArgumentMatchers.eq
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
@@ -41,7 +37,7 @@ class SupportToolbarViewTransformerTest {
         val view = Toolbar(context)
 
         transformer.apply {
-            view.transform(transformer.extractAttributes(view, getAttributeSet(false)))
+            view.transform(getAttributeSet(false))
         }
 
         view.title shouldBeEqualTo TITLE_ATTR_VALUE
@@ -52,27 +48,22 @@ class SupportToolbarViewTransformerTest {
         val view = Toolbar(context)
 
         transformer.apply {
-            view.transform(transformer.extractAttributes(view, getAttributeSet(true)))
+            view.transform(getAttributeSet(true))
         }
 
         view.title shouldBeEqualTo TITLE_ATTR_VALUE
     }
 
-    private fun getAttributeSet(withAppPrefix: Boolean): AttributeSet = mock {
-        on { attributeCount } doReturn TITLE_ATTR_INDEX + 2
-        on { getAttributeName(anyInt()) } doReturn "other_attribute"
-        on { getAttributeName(TITLE_ATTR_INDEX) } doReturn getAttributeKey(withAppPrefix)
-        on { getAttributeValue(TITLE_ATTR_INDEX) } doReturn "@$TITLE_RES_ID"
-        on { getAttributeResourceValue(eq(TITLE_ATTR_INDEX), anyInt()) } doReturn TITLE_RES_ID
-    }
+    private fun getAttributeSet(withAppPrefix: Boolean) = mapOf(
+            getAttributeKey(withAppPrefix) to TITLE_RES_ID
+    )
 
     private fun getAttributeKey(withAppPrefix: Boolean) =
             (if (withAppPrefix) "app:" else "") + TITLE_ATTR_KEY
 
     private companion object {
-        private const val TITLE_ATTR_INDEX = 3
-        private const val TITLE_RES_ID = 0x7f0f0123
         private const val TITLE_ATTR_KEY = "title"
+        private const val TITLE_RES_ID = 0x7f0f0123
         private const val TITLE_ATTR_VALUE = "TITLE_ATTR_VALUE"
     }
 }
