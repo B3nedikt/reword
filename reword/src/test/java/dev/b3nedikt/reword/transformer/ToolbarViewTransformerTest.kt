ad8a70c33b5debffa9235966f426d310d2f24681
B3nedikt <mail@b3nedikt.dev>
2020-01-09 21:32:44 +0100

    Improved unit tests

--- reword/src/test/java/dev/b3nedikt/reword/transformer/ToolbarViewTransformerTest.kt
+++ reword/src/test/java/dev/b3nedikt/reword/transformer/ToolbarViewTransformerTest.kt
@@ -3,16 +3,13 @@ package dev.b3nedikt.reword.transformer
 import android.content.Context
 import android.content.res.Resources
 import android.os.Build
-import android.util.AttributeSet
 import android.widget.Toolbar
 import androidx.test.core.app.ApplicationProvider
+import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.whenever
-import junit.framework.Assert.assertEquals
+import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers.anyInt
-import org.mockito.ArgumentMatchers.eq
-import org.mockito.Mockito.*
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 
@@ -24,10 +21,11 @@ class ToolbarViewTransformerTest {
 
     private val context: Context
         get() {
-            val context = spy<Context>(ApplicationProvider.getApplicationContext() as Context)
-            val resources = spy<Resources>(context.resources)
+            val context = com.nhaarman.mockitokotlin2.spy(ApplicationProvider.getApplicationContext() as Context)
+            val resources = com.nhaarman.mockitokotlin2.spy<Resources>(context.resources)
 
-            doReturn(resources).whenever(context).resources
+            whenever(context.resources) doReturn resources
+
             doReturn(TITLE_ATTR_VALUE).whenever(resources).getString(TITLE_RES_ID)
 
             return context
@@ -35,38 +33,36 @@ class ToolbarViewTransformerTest {
 
     @Test
     fun shouldTransformToolbar() {
-        val context = context
-
         val view = Toolbar(context)
-        transformer.apply {
-            view.transform(transformer.extractAttributes(view, getAttributeSet(false)))
-        }
-
-        assertEquals(view.title, TITLE_ATTR_VALUE)
 
         transformer.apply {
-            view.transform(transformer.extractAttributes(view, getAttributeSet(true)))
+            view.transform(getAttributeSet(false))
         }
 
-        assertEquals(view.title, TITLE_ATTR_VALUE)
+        view.title shouldBeEqualTo TITLE_ATTR_VALUE
     }
 
-    private fun getAttributeSet(withAndroidPrefix: Boolean): AttributeSet {
-        val attributeSet = mock(AttributeSet::class.java)
-        `when`(attributeSet.attributeCount).thenReturn(TITLE_ATTR_INDEX + 2)
+    @Test
+    fun shouldTransformToolbar_withAppPrefix() {
+        val view = Toolbar(context)
 
-        `when`(attributeSet.getAttributeName(anyInt())).thenReturn("other_attribute")
-        `when`(attributeSet.getAttributeName(TITLE_ATTR_INDEX)).thenReturn((if (withAndroidPrefix) "android:" else "") + TITLE_ATTR_KEY)
-        `when`(attributeSet.getAttributeValue(TITLE_ATTR_INDEX)).thenReturn("@$TITLE_RES_ID")
-        `when`(attributeSet.getAttributeResourceValue(eq(TITLE_ATTR_INDEX), anyInt())).thenReturn(TITLE_RES_ID)
+        transformer.apply {
+            view.transform(getAttributeSet(true))
+        }
 
-        return attributeSet
+        view.title shouldBeEqualTo TITLE_ATTR_VALUE
     }
 
-    companion object {
-        private const val TITLE_ATTR_INDEX = 3
-        private const val TITLE_RES_ID = 0x7f0f0123
+    private fun getAttributeSet(withAndroidPrefix: Boolean) = mapOf(
+            getAttributeKey(withAndroidPrefix) to TITLE_RES_ID
+    )
+
+    private fun getAttributeKey(withAndroidPrefix: Boolean) =
+            (if (withAndroidPrefix) "android:" else "") + TITLE_ATTR_KEY
+
+    private companion object {
         private const val TITLE_ATTR_KEY = "title"
+        private const val TITLE_RES_ID = 0x7f0f0123
         private const val TITLE_ATTR_VALUE = "TITLE_ATTR_VALUE"
     }
 }
