fe791c7677e040176bb0f6b00d65d3cf5f7a043a
B3nedikt <mail@b3nedikt.dev>
2020-01-19 18:22:41 +0100

    Improved unit tests

--- reword/src/test/java/dev/b3nedikt/reword/transformer/AbstractViewTransformerTest.kt
+++ reword/src/test/java/dev/b3nedikt/reword/transformer/AbstractViewTransformerTest.kt
@@ -2,9 +2,13 @@ package dev.b3nedikt.reword.transformer
 
 import android.os.Build
 import android.util.AttributeSet
+import android.view.View
+import androidx.test.core.app.ApplicationProvider
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.ArgumentMatchers.anyInt
 import org.robolectric.RobolectricTestRunner
@@ -14,22 +18,35 @@ import org.robolectric.annotation.Config
 @Config(sdk = [Build.VERSION_CODES.P])
 class AbstractViewTransformerTest {
 
-    private fun getAttributeSet(withAppPrefix: Boolean): AttributeSet = mock {
+    @Test
+    fun extractAttributesShouldGetTheCorrectAttributesFromTheAttributeSet() {
+        val view = View(ApplicationProvider.getApplicationContext())
+
+        ConcreteViewTransformer
+                .extractAttributes(view, getAttributeSet())[TITLE_ATTR_KEY]
+                .shouldBeEqualTo(TITLE_RES_ID)
+    }
+
+    private fun getAttributeSet(): AttributeSet = mock {
         on { attributeCount } doReturn TITLE_ATTR_INDEX + 2
         on { getAttributeName(anyInt()) } doReturn "other_attribute"
-        on { getAttributeName(TITLE_ATTR_INDEX) } doReturn getAttributeKey(withAppPrefix)
+        on { getAttributeName(TITLE_ATTR_INDEX) } doReturn TITLE_ATTR_KEY
         on { getAttributeValue(TITLE_ATTR_INDEX) } doReturn "@${TITLE_RES_ID}"
         on { getAttributeResourceValue(eq(TITLE_ATTR_INDEX), anyInt()) } doReturn TITLE_RES_ID
     }
 
-    private fun getAttributeKey(withAppPrefix: Boolean) =
-            (if (withAppPrefix) "app:" else "") + TITLE_ATTR_KEY
+    private object ConcreteViewTransformer : AbstractViewTransformer<View>() {
+
+        override val viewType = View::class.java
+
+        override val supportedAttributes: Set<String> = setOf(TITLE_ATTR_KEY)
+
+        override fun View.transform(attrs: Map<String, Int>) = Unit
+    }
 
     private companion object {
         private const val TITLE_ATTR_INDEX = 3
         private const val TITLE_RES_ID = 0x7f0f0123
         private const val TITLE_ATTR_KEY = "title"
-        private const val TITLE_ATTR_VALUE = "TITLE_ATTR_VALUE"
     }
-
 }
--- reword/src/test/java/dev/b3nedikt/reword/transformer/TextViewViewTransformerTest.kt
+++ reword/src/test/java/dev/b3nedikt/reword/transformer/TextViewViewTransformerTest.kt
@@ -3,17 +3,13 @@ package dev.b3nedikt.reword.transformer
 import android.content.Context
 import android.content.res.Resources
 import android.os.Build
-import android.util.AttributeSet
 import android.widget.EditText
 import android.widget.TextView
 import androidx.test.core.app.ApplicationProvider
-import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
-import org.junit.Assert.assertEquals
+import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.mockito.ArgumentMatchers.anyInt
-import org.mockito.ArgumentMatchers.eq
 import org.mockito.Mockito.doReturn
 import org.mockito.Mockito.spy
 import org.robolectric.RobolectricTestRunner
@@ -39,66 +35,55 @@ class TextViewViewTransformerTest {
 
     @Test
     fun shouldTransformTextView() {
-        val context = context
 
         val view = TextView(context)
         transformer.apply {
-            view.transform(transformer.extractAttributes(view, getAttributeSet(false)))
+            view.transform(getAttributeSet(false))
         }
 
-        assertEquals(view.text, TEXT_ATTR_VALUE)
-        assertEquals(view.hint, HINT_ATTR_VALUE)
+        view.text shouldBeEqualTo  TEXT_ATTR_VALUE
+        view.hint shouldBeEqualTo  HINT_ATTR_VALUE
 
         transformer.apply {
-            view.transform(transformer.extractAttributes(view, getAttributeSet(true)))
+            view.transform(getAttributeSet(true))
         }
 
-        assertEquals(view.text, TEXT_ATTR_VALUE)
-        assertEquals(view.hint, HINT_ATTR_VALUE)
+        view.text shouldBeEqualTo  TEXT_ATTR_VALUE
+        view.hint shouldBeEqualTo  HINT_ATTR_VALUE
     }
 
     @Test
     fun shouldTransformExtendedViews() {
-        val context = context
 
         val view = EditText(context)
         transformer.apply {
-            view.transform(transformer.extractAttributes(view, getAttributeSet(false)))
+            view.transform(getAttributeSet(false))
         }
 
-        assertEquals(view.text.toString(), TEXT_ATTR_VALUE)
-        assertEquals(view.hint, HINT_ATTR_VALUE)
+        view.text.toString() shouldBeEqualTo TEXT_ATTR_VALUE
+        view.hint shouldBeEqualTo HINT_ATTR_VALUE
 
         transformer.apply {
-            view.transform(transformer.extractAttributes(view, getAttributeSet(true)))
+            view.transform(getAttributeSet(true))
         }
 
-        assertEquals(view.text.toString(), TEXT_ATTR_VALUE)
-        assertEquals(view.hint, HINT_ATTR_VALUE)
+        view.text.toString() shouldBeEqualTo TEXT_ATTR_VALUE
+        view.hint shouldBeEqualTo HINT_ATTR_VALUE
     }
 
-    private fun getAttributeSet(withAndroidPrefix: Boolean): AttributeSet {
-        val attributeSet = mock<AttributeSet>()
-        whenever(attributeSet.attributeCount).thenReturn(TEXT_ATTR_INDEX + 2)
+    private fun getAttributeSet(withAndroidPrefix: Boolean) = mapOf(
+            getAttributeKey(withAndroidPrefix, TEXT_ATTR_KEY) to TEXT_RES_ID,
+            getAttributeKey(withAndroidPrefix, HINT_ATTR_KEY) to HINT_RES_ID
+    )
 
-        whenever(attributeSet.getAttributeName(anyInt())).thenReturn("other_attribute")
-        whenever(attributeSet.getAttributeName(TEXT_ATTR_INDEX)).thenReturn((if (withAndroidPrefix) "android:" else "") + TEXT_ATTR_KEY)
-        whenever(attributeSet.getAttributeValue(TEXT_ATTR_INDEX)).thenReturn("@$TEXT_RES_ID")
-        whenever(attributeSet.getAttributeResourceValue(eq(TEXT_ATTR_INDEX), anyInt())).thenReturn(TEXT_RES_ID)
-        whenever(attributeSet.getAttributeName(HINT_ATTR_INDEX)).thenReturn((if (withAndroidPrefix) "android:" else "") + HINT_ATTR_KEY)
-        whenever(attributeSet.getAttributeValue(HINT_ATTR_INDEX)).thenReturn("@$HINT_RES_ID")
-        whenever(attributeSet.getAttributeResourceValue(eq(HINT_ATTR_INDEX), anyInt())).thenReturn(HINT_RES_ID)
-
-        return attributeSet
-    }
+    private fun getAttributeKey(withAndroidPrefix: Boolean, key: String) =
+            (if (withAndroidPrefix) "android:" else "") + key
 
     private companion object {
-        private const val TEXT_ATTR_INDEX = 3
         private const val TEXT_RES_ID = 0x7f0f0123
         private const val TEXT_ATTR_KEY = "text"
         private const val TEXT_ATTR_VALUE = "TEXT_ATTR_VALUE"
 
-        private const val HINT_ATTR_INDEX = 2
         private const val HINT_RES_ID = 0x7f0f0124
         private const val HINT_ATTR_KEY = "hint"
         private const val HINT_ATTR_VALUE = "HINT_ATTR_VALUE"
