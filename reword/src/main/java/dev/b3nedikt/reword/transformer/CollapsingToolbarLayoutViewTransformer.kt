dfe1b30a4bf8a6714c7de60eefec896458fc0e14
B3nedikt <mail@b3nedikt.dev>
2020-10-03 14:47:14 +0200

    Implemented support for the CollapsingToolbarLayout

---
+++ reword/src/main/java/dev/b3nedikt/reword/transformer/CollapsingToolbarLayoutViewTransformer.kt
@@ -0,0 +1,24 @@
+package dev.b3nedikt.reword.transformer
+
+import com.google.android.material.appbar.CollapsingToolbarLayout
+
+/**
+ * A transformer which transforms the title of a [CollapsingToolbarLayout]
+ */
+internal object CollapsingToolbarLayoutViewTransformer : AbstractViewTransformer<CollapsingToolbarLayout>() {
+
+    private const val ATTRIBUTE_TITLE = "text"
+    private const val ATTRIBUTE_ANDROID_TITLE = "android:text"
+
+    override val viewType = CollapsingToolbarLayout::class.java
+
+    override val supportedAttributes = setOf(ATTRIBUTE_TITLE, ATTRIBUTE_ANDROID_TITLE)
+
+    override fun CollapsingToolbarLayout.transform(attrs: Map<String, Int>) {
+        attrs.forEach { entry ->
+            when (entry.key) {
+                ATTRIBUTE_ANDROID_TITLE, ATTRIBUTE_TITLE -> updateTexts(entry.value, this::setTitle)
+            }
+        }
+    }
+}
---
+++ reword/src/test/java/dev/b3nedikt/reword/transformer/CollapsingToolbarLayoutTransformerTest.kt
@@ -0,0 +1,62 @@
+package dev.b3nedikt.reword.transformer
+
+import android.content.Context
+import android.os.Build
+import androidx.test.core.app.ApplicationProvider
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.nhaarman.mockitokotlin2.whenever
+import dev.b3nedikt.reword.TestApplication
+import org.amshove.kluent.shouldBeEqualTo
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
+
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.P], application = TestApplication::class)
+class CollapsingToolbarLayoutTransformerTest {
+
+    private var transformer = CollapsingToolbarLayoutViewTransformer
+
+    private val context: Context
+        get() {
+            val context = Mockito.spy(ApplicationProvider.getApplicationContext() as Context)
+            val resources = Mockito.spy(context.resources)
+
+            whenever(context.resources).thenReturn(resources)
+            Mockito.doReturn(TITLE_ATTR_VALUE).whenever(resources).getString(TITLE_RES_ID)
+
+            return context
+        }
+
+    @Test
+    fun shouldTransformCollapsingToolbarLayout() {
+
+        val view = CollapsingToolbarLayout(context)
+        transformer.apply {
+            view.transform(getAttributeSet(false))
+        }
+
+        view.title shouldBeEqualTo  TITLE_ATTR_VALUE
+
+        transformer.apply {
+            view.transform(getAttributeSet(true))
+        }
+
+        view.title shouldBeEqualTo  TITLE_ATTR_VALUE
+    }
+
+    private fun getAttributeSet(withAndroidPrefix: Boolean) = mapOf(
+            getAttributeKey(withAndroidPrefix) to TITLE_RES_ID,
+    )
+
+    private fun getAttributeKey(withAndroidPrefix: Boolean) =
+            (if (withAndroidPrefix) "android:" else "") + TITLE_ATTR_KEY
+
+    private companion object {
+        private const val TITLE_RES_ID = 0x7f0f0123
+        private const val TITLE_ATTR_KEY = "text"
+        private const val TITLE_ATTR_VALUE = "TEXT_ATTR_VALUE"
+    }
+}
