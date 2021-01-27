2f5b76c322b3858472c18a75166c6efe5b78e196
B3nedikt <mail@b3nedikt.dev>
2021-01-27 22:58:27 +0100

    Reworked the ViewTransformer API

--- reword/src/main/java/dev/b3nedikt/reword/transformer/AbstractViewTransformer.kt
+++ reword/src/main/java/dev/b3nedikt/reword/transformer/AbstractViewTransformer.kt
@@ -1,23 +1,11 @@
 package dev.b3nedikt.reword.transformer
 
-import android.util.AttributeSet
 import android.view.View
 
 /**
  * A [ViewTransformer] skeleton.
  */
 abstract class AbstractViewTransformer<T : View> : ViewTransformer<T> {
-
-    /**
-     * Extracts the attributes of a [View] from its [AttributeSet]
-     *
-     * @param view  we want to update the text of
-     * @param attrs attributes of the view.
-     * @return a [Map] of attributes as keys and their value
-     */
-    override fun extractAttributes(view: View, attrs: AttributeSet): Map<String, Int> {
-        return attrs.extractAttributes(supportedAttributes)
-    }
 
     /**
      * Update the texts of a view.
